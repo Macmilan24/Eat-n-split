69daf0f2bfb73653efa35bd0d62685ac98bea0e7
Macmilan24 <cpt.macmilan24@gmail.com>
2024-05-11 10:25:19 +0300

    finshed the demo friend list

--- src/App.js
+++ src/App.js
@@ -42,5 +42,23 @@ function FriendsList() {
 }
 
 function Friend({ friend }) {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes You {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
