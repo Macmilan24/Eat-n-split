8cea02d0e035de9b83c7a9518b5ed3cf5296e244
Macmilan24 <cpt.macmilan24@gmail.com>
2024-05-11 10:14:04 +0300

    created a friend list commponet

--- src/App.js
+++ src/App.js
@@ -20,5 +20,15 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  return <ul>List</ul>;
 }
